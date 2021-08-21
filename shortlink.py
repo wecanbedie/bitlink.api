ca5af47dc0f18b7a7bb3cd32f99c9edbc94ad51c
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2021-08-21 13:55:03 +0300

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 import argparse
 
 
-
-def count_clicks(token_header, pars_link):
-    parsed_url = urlparse(pars_link)
+def count_clicks(token_header, link):
+    parsed_url = urlparse(link)
     path = parsed_url.path
     netloc = parsed_url.netloc
     summary_click_check_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}{}/clicks/summary'
@@ -16,16 +15,16 @@ def count_clicks(token_header, pars_link):
     return response.json()['total_clicks']
 
 
-def shorten_link(token_header, pars_link):
+def shorten_link(token_header, link):
     shorten_link_create = 'https://api-ssl.bitly.com/v4/bitlinks'
-    payload = {'long_url': pars_link}
+    payload = {'long_url': link}
     response = requests.post(shorten_link_create, headers=token_header, json=payload)
     response.raise_for_status()
     return response.json()['link']
 
 
-def check_bitly(token_header, pars_link):
-    parsed_url = urlparse(pars_link)
+def check_bitly(token_header, link):
+    parsed_url = urlparse(link)
     path = parsed_url.path
     netloc = parsed_url.netloc
     check_bitlink = 'https://api-ssl.bitly.com/v4/bitlinks/{}{}'
@@ -37,15 +36,15 @@ if __name__ == '__main__':
     load_dotenv()
     bitly_token = os.environ['BITLY_TOKEN']
     token_header = {'Authorization': 'Bearer {}'.format(bitly_token)}
-    parser = argparse.ArgumentParser()
-    parser.add_argument('link', type=str, help='Мне нужна ссылка для вывода (битлинка/количества кликов)')
-    pars_link = parser.parse_args().link
+    parser = argparse.ArgumentParser(description='Сокращает ссылки и считает количество кликов ')
+    parser.add_argument('link', type=str, help='Введите ссылку для сокращения')
+    link = parser.parse_args().link
     try:
-        if check_bitly(token_header, pars_link):
-            shared_clicks = count_clicks(token_header, pars_link)
+        if check_bitly(token_header, link):
+            shared_clicks = count_clicks(token_header, link)
             print('Количество кликов: ', shared_clicks)
         else:
-            shorten_link_create = shorten_link(token_header, pars_link)
+            shorten_link_create = shorten_link(token_header, link)
             print('Битлинк:', shorten_link_create)
     except requests.exceptions.HTTPError:
         print('Ссылка не является действительной')
